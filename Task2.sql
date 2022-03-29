c647f7d7beda1364566ad75b97c55c8a02d70d48
andrey-rsn <korowai98.ag@gmail.com>
2022-03-29 18:53:01 +0300

    AddedTask2

---
+++ Task2.sql
@@ -0,0 +1,8 @@
+﻿--products (таблица продуктов)
+--categories (таблица категорий)
+--productsCategories (таблица категорий продуктов (отношение многие ко многим) )
+
+select products.product_name,categories.category_name from productsCategories 
+full outer join products on productsCategories.product_id=products.id
+full outer join categories on productsCategories.category_id=categories.id
+
